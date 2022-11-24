0c38d08f78739375797a6c4f7ef0883cdde50249
Michael Strobel <mstrobel97@gmail.com>
2022-11-24 13:28:42 +0100

    add new tests; add additional rules

--- tests/sample/index.php
+++ tests/sample/index.php
@@ -2,31 +2,28 @@
 /* Test format */
 
 // RESULT OF JOIN (node "array_creation_expression", preset default)
-$array = ['test' => 15, '12' => 'Hello', 'World' => '!'];
+$array = ['test' => 15, '12' => 'Hello', 'World' => [1, 2, 3]];
 
 // RESULT OF SPLIT (node "array_creation_expression", preset default)
 $array = [
-    'test' => 15,
-    '12' => 'Hello',
-    'World' => '!',
+  'test' => 15,
+  '12' => 'Hello',
+  'World' => [1, 2, 3],
 ];
 
-// RESULT OF JOIN (node "array_creation_expression", preset default)
-$array = [1, 2, 3, 4, 5, [6, 7, 8, 9]];
+// RESULT OF JOIN (node "arguments", preset default)
+test(1, 2, 3);
 
-// RESULT OF SPLIT (node "array_creation_expression", preset default)
-$array = [
-    1,
-    2,
-    3,
-    4,
-    5,
-    [6, 7, 8, 9],
-];
+// RESULT OF SPLIT (node "arguments", preset default)
+test(
+  1,
+  2,
+  3
+);
 
 // RESULT OF JOIN (node "formal_parameters", preset default)
 function test($a, $b, $c) {
-    print_r([$a, $b, $c]);
+  print_r([$a, $b, $c]);
 }
 
 // RESULT OF SPLIT (node "formal_parameters", preset default)
@@ -35,36 +32,26 @@ function test(
   $b,
   $c
 ) {
-    print_r([$a, $b, $c]);
+  print_r([$a, $b, $c]);
 }
-
-// RESULT OF JOIN (node "arguments", preset default)
-test(1, 2, 3);
-
-// RESULT OF SPLIT (node "arguments", preset default)
-test(
-    1,
-    2,
-    3
-);
 
 // RESULT OF JOIN (node "compound_statement", preset default)
 function state($a, $b, $c) { print_r($a); function sum($n1, $n2) { return $n1 + $n2; } $res = sum($b, $c); return $res; }
 
 // RESULT OF SPLIT (node "statement_block", preset default)
 function state($a, $b, $c) {
-    print_r($a);
-    function sum($n1, $n2) { return $n1 + $n2; }
-    $res = sum($b, $c);
-    return $res;
+  print_r($a);
+  function sum($n1, $n2) { return $n1 + $n2; }
+  $res = sum($b, $c);
+  return $res;
 }
 
-// RESULT OF JOIN (node "expression_statement" with target "array_creation_expression", preset default)
+// RESULT OF JOIN (node "assignment_expression" with target "array_creation_expression", preset default)
 $array = ['test' => 15, '12' => 'Hello', 'World' => '!'];
 
-// RESULT OF SPLIT (node "lexical_declaration" with target "object", preset default)
+// RESULT OF SPLIT (node "assignment_expression" with target "array_creation_expression", preset default)
 $array = [
-    'test' => 15,
-    '12' => 'Hello',
-    'World' => '!',
+  'test' => 15,
+  '12' => 'Hello',
+  'World' => '!',
 ];
