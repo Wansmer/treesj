a5f3ec981f78bbeaccb01ecf876be451f4111122
Wansmer <wansmer@gmail.com>
2022-11-24 09:14:47 +0300

    fix #8

--- lua/treesj/langs/tsx.lua
+++ lua/treesj/langs/tsx.lua
@@ -1,9 +1,15 @@
 local u = require('treesj.langs.utils')
+local ts = require('treesj.langs.typescript')
 
-return {
+return u.merge_preset(ts, {
   jsx_opening_element = u.set_default_preset({
     both = {
       omit = { 'identifier' },
+    },
+  }),
+  jsx_element = u.set_default_preset({
+    join = {
+      space_separator = 0,
     },
   }),
   jsx_self_closing_element = u.set_default_preset({
@@ -12,4 +18,4 @@ return {
       no_format_with = {},
     },
   }),
-}
+})
