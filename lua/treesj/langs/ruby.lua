00d3ef0853e97263d7d1396b28cc8fd8bb427df9
Wansmer <wansmer@gmail.com>
2022-11-28 17:15:49 +0300

    Delete extra nodes from preset

--- lua/treesj/langs/ruby.lua
+++ lua/treesj/langs/ruby.lua
@@ -5,6 +5,7 @@ return {
   hash = u.set_preset_for_list(),
   method_parameters = u.set_preset_for_args(),
   argument_list = u.set_preset_for_args(),
-  assignment = { target_nodes = { 'array', 'hash' } },
-  method = { target_nodes = { 'body_statement' } },
+  assignment = {
+    target_nodes = { 'array', 'hash' },
+  },
 }
