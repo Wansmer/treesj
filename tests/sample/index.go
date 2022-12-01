9c37277b16c2180fe117649673c2ca6cb99e096c
Wansmer <wansmer@gmail.com>
2022-12-01 21:05:36 +0300

    Added Go

---
+++ tests/sample/index.go
@@ -0,0 +1,55 @@
+// code examples from https://learnxinyminutes.com/docs/go/ and stackoverflow
+
+// RESULT OF JOIN (block "literal_value" list, preset default)
+a5 := [...]int{ 3, 1, 5, 10, 100 }
+
+// RESULT OF SPLIT (block "literal_value" list, preset default)
+a5 := [...]int{
+  3,
+  1,
+  5,
+  10,
+  100,
+}
+
+// RESULT OF JOIN (block "literal_value" dict, preset default)
+joe := person{ name: "Doe, John", age:  32 }
+
+// RESULT OF SPLIT (block "literal_value" dict, preset default)
+joe := person{
+  name: "Doe, John",
+  age:  32,
+}
+
+// RESULT OF JOIN (block "block", preset default)
+func main() { fmt.Println("Hello world!"); beyondHello() }
+
+// RESULT OF SPLIT (block "block", preset default)
+func main() {
+  fmt.Println("Hello world!");
+  beyondHello()
+}
+
+// RESULT OF JOIN (block "parameter_list", preset default)
+func learnMultiple(x, y int, z, some) (sum, prod int, x, y) {
+  return x + y, x * y
+}
+
+// RESULT OF SPLIT (block "parameter_list", preset default)
+func learnMultiple(
+  x, y int,
+  z,
+  some,
+) (sum, prod int, x, y) {
+  return x + y, x * y
+}
+
+// RESULT OF JOIN (block "argument_list", preset default)
+learnErrorHandling(1, 2, 3)
+
+// RESULT OF JOIN (block "argument_list", preset default)
+learnErrorHandling(
+  1,
+  2,
+  3,
+)
