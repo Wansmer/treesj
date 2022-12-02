3d332c7456c25ea29c153bbd65103f697f6c1bda
Wansmer <wansmer@gmail.com>
2022-12-02 10:51:47 +0300

    Added pug

---
+++ lua/treesj/langs/pug.lua
@@ -0,0 +1,12 @@
+local u = require('treesj.langs.utils')
+
+return {
+  attributes = u.set_default_preset({
+    both = {
+      separator = ' ',
+    },
+  }),
+  tag = {
+    target_nodes = { 'attributes' },
+  },
+}
