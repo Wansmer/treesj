4d50281d534d06766caa6626809b8036766385a0
이성민(나사공) <60088301+boltlessengineer@users.noreply.github.com>
2023-02-22 16:16:55 +0900

    feat: support jsonc (#65)

---
+++ lua/treesj/langs/jsonc.lua
@@ -0,0 +1,14 @@
+local u = require('treesj.langs.utils')
+
+return {
+  object = u.set_preset_for_dict({
+    split = {
+      last_separator = false,
+    },
+  }),
+  array = u.set_preset_for_list({
+    split = {
+      last_separator = false,
+    },
+  }),
+}
