94aaba7ae2d2a1e967bed3d5b16964d19c5edc77
Wansmer <wansmer@gmail.com>
2023-02-28 15:10:54 +0300

    Added preset for `let_expression`

--- lua/treesj/langs/nix.lua
+++ lua/treesj/langs/nix.lua
@@ -4,7 +4,7 @@ return {
   list_expression = u.set_preset_for_list({
     both = {
       separator = '',
-    }
+    },
   }),
   attrset_expression = {
     target_nodes = { 'binding_set' },
@@ -18,7 +18,7 @@ return {
   }),
   formals = u.set_preset_for_args({
     both = {
-      omit = { 'formal', 'ellipses' }
+      omit = { 'formal', 'ellipses' },
     },
     split = {
       separator = '',
@@ -27,6 +27,20 @@ return {
     join = {
       separator = ',',
       space_in_brackets = true,
+    },
+  }),
+  let_expression = u.set_default_preset({
+    both = {
+      omit = { 'binding_set' },
+    },
+    split = {
+      recursive = true,
+      inner_indent = 'normal',
+      last_indent = 'inner',
+    },
+    join = {
+      space_in_brackets = true,
+      space_separator = 1,
     },
   }),
 }
