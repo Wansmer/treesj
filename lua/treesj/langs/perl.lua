fbd93bb3d3f4efa7a96633a4e307aa0e1ecc63f7
Ivan Smirnov <46977173+Wansmer@users.noreply.github.com>
2023-11-15 12:56:02 +0300

    Fix: update perl (#141)
    * add new names of nodes
    
    * update test annotations in sample
    
    * update test annotations in spec

--- lua/treesj/langs/perl.lua
+++ lua/treesj/langs/perl.lua
@@ -1,7 +1,35 @@
 local lang_utils = require('treesj.langs.utils')
 
 return {
-  array = lang_utils.set_preset_for_args({
+  list_expression = lang_utils.set_preset_for_non_bracket({
+    both = {
+      last_separator = false,
+    },
+    split = {
+      separator = ',',
+      omit = {
+        '=>',
+        function(child)
+          local prev = child:prev()
+          return prev and prev:type() == '=>'
+        end,
+      },
+    },
+    join = {
+      space_in_brackets = false,
+    },
+  }),
+  anonymous_array_expression = { target_nodes = { 'list_expression' } },
+  anonymous_hash_expression = { target_nodes = { 'list_expression' } },
+  block = lang_utils.set_preset_for_statement({
+    both = {
+      omit = { 'semi_colon' },
+    },
+  }),
+  variable_declaration = {
+    target_nodes = { 'array', 'array_ref', 'hash_ref' },
+  },
+  array = lang_utils.set_preset_for_args({ -- Not relevant. Left for compatibility with previous versions of the parser
     split = {
       omit = {
         '=>',
@@ -12,22 +40,14 @@ return {
       },
     },
   }),
-  hash_ref = lang_utils.set_preset_for_dict({
+  hash_ref = lang_utils.set_preset_for_dict({ -- Not relevant. Left for compatibility with previous versions of the parser
     join = {
       space_in_brackets = false,
     },
   }),
-  array_ref = lang_utils.set_preset_for_dict({
+  array_ref = lang_utils.set_preset_for_dict({ -- Not relevant. Left for compatibility with previous versions of the parser
     join = {
       space_in_brackets = false,
     },
   }),
-  block = lang_utils.set_preset_for_statement({
-    both = {
-      omit = { 'semi_colon' },
-    },
-  }),
-  variable_declaration = {
-    target_nodes = { 'array', 'array_ref', 'hash_ref' },
-  },
 }
