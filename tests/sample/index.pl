24394118378a679c7f029d71e68a6548553c5ba3
Wansmer <wansmer@gmail.com>
2023-11-15 12:52:16 +0300

    update test annotations in sample

--- tests/sample/index.pl
+++ tests/sample/index.pl
@@ -1,36 +1,36 @@
 # Code examples from here: https://learnxinyminutes.com/docs/perl/
 
-# RESULT OF JOIN (node "array", preset default)
+# RESULT OF JOIN (node "list_expression", preset default)
 my @mixed = ("camel", 42, 1.23);
-# RESULT OF SPLIT (node "array", preset default)
+# RESULT OF SPLIT (node "list_expression", preset default)
 my @mixed = (
   "camel",
   42,
   1.23
 );
 
-# RESULT OF JOIN (node "array" with "=>" syntax, preset default)
+# RESULT OF JOIN (node "list_expression" with "=>" syntax, preset default)
 my %fruit_color = (apple => "red", banana => "yellow");
-# RESULT OF SPLIT (node "array" with "=>" syntax, preset default)
+# RESULT OF SPLIT (node "list_expression" with "=>" syntax, preset default)
 my %fruit_color = (
   apple => "red",
   banana => "yellow"
 );
 
-# RESULT OF JOIN (node "array_ref", preset default)
+# RESULT OF JOIN (node "list_expression", preset default)
 my $fruits = ["apple", "banana"];
-# RESULT OF SPLIT (node "array_ref", preset default)
+# RESULT OF SPLIT (node "list_expression", preset default)
 my $fruits = [
   "apple",
-  "banana",
+  "banana"
 ];
 
-# RESULT OF JOIN (node "hash_ref", preset default)
+# RESULT OF JOIN (node "list_expression", preset default)
 my $colors = {apple => "red", banana => "yellow"};
-# RESULT OF SPLIT (node "hash_ref", preset default)
+# RESULT OF SPLIT (node "list_expression", preset default)
 my $colors = {
   apple => "red",
-  banana => "yellow",
+  banana => "yellow"
 };
 
 # RESULT OF JOIN (node "block", preset default)
