d089d345703633cc35c460316e2a5687ec3fdab2
Paul Gideon Dann <pgdann@gmail.com>
2024-12-10 12:28:25 +0000

    fix: elixir: pad everything in keywords except strings (#168)
    It's safer to accidentally introduce extra padding that isn't needed
    than to not include padding that is, because missing padding actually
    breaks the syntax.

--- lua/treesj/langs/elixir.lua
+++ lua/treesj/langs/elixir.lua
@@ -35,8 +35,8 @@ return {
   keywords = lang_utils.set_preset_for_list({
     both = {
       non_bracket_node = true,
-      -- This is only needed because for some reason join and
-      -- split on a keyword where the value is a map goes like this:
+      -- This is only needed because for some reason join and split on a keyword
+      -- where the value is certain types of term looks like:
       -- expected: 'key: %{ a: "b" }'
       -- result: 'key:%{ a: "b" }'
       format_tree = function(tsj)
@@ -44,15 +44,14 @@ return {
           local pairs = tsj:children({ 'pair' })
           for _, pair in ipairs(pairs) do
             for keyword in pair:iter_children() do
-              local type = keyword:type()
-              local needs_padding =
-                type == 'map' or type == 'list' or type == 'tuple'
-              if needs_padding then
+              if keyword:type() ~= 'string' then
                 -- Grab the previous node which is the map key
                 local map_key = keyword:prev()
-                -- Add an extra space to account for keyword + map quirkness
-                local text = map_key:text():gsub(':$', ': ')
-                map_key:update_text(text)
+                if map_key then
+                  -- Add an extra space to account for keyword + value quirkness
+                  local text = map_key:text():gsub(':$', ': ')
+                  map_key:update_text(text)
+                end
               end
             end
           end
