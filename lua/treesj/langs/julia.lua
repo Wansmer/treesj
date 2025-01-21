7eff93647ba72b523d48dcc5d092464705bccce5
Wansmer (Ivan Smirnov) <wansmer@gmail.com>
2025-01-21 18:04:15 +0300

    feat: open_tuple and tuple_expression

--- lua/treesj/langs/julia.lua
+++ lua/treesj/langs/julia.lua
@@ -19,7 +19,19 @@ return {
     },
   }),
   tuple_expression = lang_utils.set_preset_for_list({
-    join = { space_in_brackets = false },
+    join = {
+      space_in_brackets = false,
+      format_tree = function(tsj)
+        if
+          tsj:tsnode():parent():type() == 'assignment'
+          -- Check if the tuple_expression is the left side in the assignment
+          and tsj:tsnode():parent():child(0):equal(tsj:tsnode())
+        then
+          tsj:remove_child({ '(', ')' })
+          tsj:child(-1):update_text(' ' .. tsj:child(-1):text())
+        end
+      end,
+    },
   }),
   comprehension_expression = lang_utils.set_preset_for_list({
     both = {
@@ -33,6 +45,20 @@ return {
   using_statement = lang_utils.set_preset_for_non_bracket({
     both = {
       omit = { lang_utils.helpers.if_second },
+    },
+  }),
+  open_tuple = lang_utils.set_preset_for_args({
+    split = {
+      last_separator = true,
+      format_tree = function(tsj)
+        tsj:create_child({ text = '(' }, 1)
+        tsj:create_child({ text = ')' }, #tsj:children() + 1)
+        local penult = tsj:child(-2)
+        penult:update_text(penult:text() .. ',')
+      end,
+    },
+    join = {
+      disable = true,
     },
   }),
 }
